cbcbf99182b3d727a0462b0d8bfb626354b5c9b0
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2016-03-30 18:59:29 -0600

    added rest of folders

---
+++ week9/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="../style.css" />
+    <style>
+			.container{
+				width:95%;
+			}
+    </style>
+	<title>Video</title>
+</head>
+
+<body>
+	<div >
+
+    <div class="row mainContainer">
+        <div class="col-sm-2">
+						<?php include '../menu.php';?>
+        </div>
+
+        <div class="col-sm-10">
+            <div  class="container">
+							<br>
+                <h2>Filming in Progress...</h2>
+
+            </div>
+				</div>
+    </div>
+	</div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="../script.js"></script>
+		<script >setActive(8)</script>
+</body>
+
+</html>
